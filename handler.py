965dd679a01c83766e687064662d275bb5975de5
casadamente <adm@casadamente.com.br>
2025-12-06 14:31:02 -0300

    fix: Disable numba caching to fix librosa issue

--- handler.py
+++ handler.py
@@ -13,6 +13,7 @@ Data: 2025-12-06
 # Fix numba/librosa caching issue - MUST be before any imports
 import os
 os.environ['NUMBA_CACHE_DIR'] = '/tmp/numba_cache'
+os.environ['NUMBA_DISABLE_CACHING'] = '1'
 os.environ['MPLCONFIGDIR'] = '/tmp/mpl'
 
 import runpod
